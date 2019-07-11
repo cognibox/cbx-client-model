7155d8dc016a4c644cc1e9afca5ff61b532de9fc
decaruju <julien.decarufel@gmail.com>
2019-07-11 10:51:00 -0400

    association validation (#40)

--- test/mixins/validation.js
+++ test/mixins/validation.js
@@ -3,22 +3,118 @@ import BaseModel from '../../lib/model.js';
 import ValidationMixin from '../../lib/mixins/validation.js';
 
 describe('Validation', () => {
-  let ModelWithValidation;
+  const longEnoughValidationText = 'Name is not long enough', atLeastOneValidationText = 'Need more assoc';
+  let ModelWithValidation, AssociationModel;
 
   beforeEach(() => {
+    AssociationModel = class extends BaseModel {
+      static attributes() {
+        return {
+          id: {},
+        };
+      }
+    };
+
     ModelWithValidation = class extends ValidationMixin(BaseModel) {
       static attributes() {
         return {
           name: {
             validations: {
               isLongEnough(value) {
-                return value.length > 10;
+                if (value.length > 10) return true;
+
+                return longEnoughValidationText;
+              },
+            },
+          },
+        };
+      }
+
+      static associations() {
+        return {
+          assoc: {
+            class: AssociationModel,
+            default: [],
+            type: 'hasMany',
+            validations: {
+              hasAtLeastOne(value) {
+                if (value.length > 0) return true;
+
+                return atLeastOneValidationText;
               },
             },
           },
         };
       }
     };
+  });
+
+  context('when a model is validated', () => {
+    let model;
+
+    context('when its attributes are valid', () => {
+      context('when its associations are valid', () => {
+        beforeEach(() => {
+          model = new ModelWithValidation({ name: 'a very long name', assoc: [new AssociationModel()] });
+          model.validate();
+        });
+
+        it('should set isValid to true', () => {
+          expect(model.isValid).to.be.true;
+        });
+
+        it('should set errors to an empty object', () => {
+          expect(model.errors).to.be.an('object').that.is.empty;
+        });
+      });
+
+      context('when its associations are not valid', () => {
+        beforeEach(() => {
+          model = new ModelWithValidation({ name: 'a very long name', ass: [] });
+          model.validate();
+        });
+
+        it('should set isValid to false', () => {
+          expect(model.isValid).to.be.false;
+        });
+
+        it('should set errors', () => {
+          expect(model.errors).to.deep.equal({ assoc: { hasAtLeastOne: atLeastOneValidationText } });
+        });
+      });
+    });
+
+    context('when its attributes are not valid', () => {
+      context('when its associations are valid', () => {
+        beforeEach(() => {
+          model = new ModelWithValidation({ name: 'shortname', assoc: [new AssociationModel()] });
+          model.validate();
+        });
+
+        it('should set isValid to false', () => {
+          expect(model.isValid).to.be.false;
+        });
+
+        it('should set errors', () => {
+          expect(model.errors).to.deep.equal({ name: { isLongEnough: longEnoughValidationText } });
+        });
+      });
+
+      context('when its associations are not valid', () => {
+        beforeEach(() => {
+          model = new ModelWithValidation({ name: 'shortname', assoc: [] });
+          model.validate();
+        });
+
+        it('should set isValid to false', () => {
+          expect(model.isValid).to.be.false;
+        });
+
+        it('should set errors', () => {
+          expect(model.errors).to.deep.equal({ name: { isLongEnough: longEnoughValidationText }, assoc: { hasAtLeastOne: atLeastOneValidationText } });
+        });
+      });
+    });
   });
 
   context('when an attribute is validated', () => {
@@ -41,7 +137,7 @@ describe('Validation', () => {
           });
 
           it('should set errors', () => {
-            expect(model.errors).to.deep.equal({ name: { isLongEnough: false } });
+            expect(model.errors).to.deep.equal({ name: { isLongEnough: longEnoughValidationText } });
           });
         });
 
@@ -79,7 +175,7 @@ describe('Validation', () => {
           });
 
           it('should set errors', () => {
-            expect(model.errors).to.deep.equal({ name: { isLongEnough: false } });
+            expect(model.errors).to.deep.equal({ name: { isLongEnough: longEnoughValidationText } });
           });
         });
 
