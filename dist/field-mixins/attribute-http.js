c6e74f972b67816bc14fb9539c6b5cffddf869bc
Pascal Godin <godin_boy@msn.com>
2020-01-31 12:12:41 -0500

    release 4.10.0

---
+++ dist/field-mixins/attribute-http.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+
+var _possibleConstructorReturn2 = _interopRequireDefault(require("@babel/runtime/helpers/possibleConstructorReturn"));
+
+var _getPrototypeOf2 = _interopRequireDefault(require("@babel/runtime/helpers/getPrototypeOf"));
+
+var _inherits2 = _interopRequireDefault(require("@babel/runtime/helpers/inherits"));
+
+var _default = function _default(superclass) {
+  return (
+    /*#__PURE__*/
+    function (_superclass) {
+      (0, _inherits2["default"])(_class, _superclass);
+
+      function _class() {
+        (0, _classCallCheck2["default"])(this, _class);
+        return (0, _possibleConstructorReturn2["default"])(this, (0, _getPrototypeOf2["default"])(_class).apply(this, arguments));
+      }
+
+      (0, _createClass2["default"])(_class, [{
+        key: "httpParse",
+        value: function httpParse() {
+          return this.value;
+        }
+      }]);
+      return _class;
+    }(superclass)
+  );
+};
+
+exports["default"] = _default;
